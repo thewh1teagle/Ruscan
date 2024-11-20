aed9b9012a107d5566387de2828c3d756e9c9839
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-11-20 10:57:28 +0200

    upgrade packages

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 mod cmd;
 
 fn main() {
-    env_logger::init();
+    tracing_subscriber::fmt()
+        .with_line_number(true)
+        .with_file(true)
+        .init();
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![cmd::scan, cmd::get_interfaces])
         .run(tauri::generate_context!())
